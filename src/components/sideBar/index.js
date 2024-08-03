225596b7687bb973575a334fe50ca031f2a92de4
Bin-Ne <vinhvlog2108@gmail.com>
2024-08-03 11:16:11 +0700

    Commit FrontEnd Source

---
+++ src/components/sideBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SideBar.module.scss';
+
+const SideBar = () => {
+    return (
+        <div className={styles.sidebar}>
+            <h1>SideBar</h1>
+        </div>
+    );
+};
+
+export default SideBar;
